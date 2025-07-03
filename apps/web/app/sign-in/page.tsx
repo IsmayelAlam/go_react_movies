2a3ae19b2592fbfcfc929ec8cabd421a7a9747eb
Ismayel Alam <ismayelalam0@gmail.com>
2025-07-03 21:17:43 +0200

    create auth types and add log in page

--- apps/web/app/sign-in/page.tsx
+++ apps/web/app/sign-in/page.tsx
@@ -1,5 +1,20 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@radix-ui/react-label';
 import React from 'react';
 
 export default function Auth() {
-  return <div>Auth</div>;
+  return (
+    <form className="space-y-4 max-w-md mx-auto mt-10 p-6">
+      <div className="flex flex-col gap-1">
+        <Label htmlFor="email">Email</Label>
+        <Input id="email" />
+      </div>
+      <div className="flex flex-col gap-1">
+        <Label htmlFor="password">Password</Label>
+        <Input id="password" />
+      </div>
+      <Button>Sign In</Button>
+    </form>
+  );
 }
